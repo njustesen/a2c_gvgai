dfdc5b0d0fcd9d3cd9358409b5c8eb7560b34d44
root <root@soreubupubrrt001.tccbdzlamjle3djzcx1zvye01d.ex.internal.cloudapp.net>
2018-05-10 19:23:34 +0000

    new installation file

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -9,24 +9,24 @@ sudo apt-get install python3-pip python3-dev
 sudo apt-get install git cmake libopenmpi-dev python3-dev zlib1g-dev
 
 #Install image processing libraries
-pip3 install pillow
-pip3 install opencv-python
+sudo pip3 install pillowsudo 
+sudo pip3 install opencv-python
 
 #Install Tensorflow
 #pip3 install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.8.0-cp36-cp36m-linux_x86_64.whl
-pip3 install tensorflow_gpu
+sudo pip3 install tensorflow_gpu
 
 #Install GVGAI-GYM
-git clone https://github.com/rubenrtorrado/GVGAI_GYM.git
-pip3 install -e ./GVGAI_GYM
+sudo git clone https://github.com/rubenrtorrado/GVGAI_GYM.git
+sudo pip3 install -e ./GVGAI_GYM
 
 #Install Baselines
-git clone https://github.com/openai/baselines.git
-pip3 install -e ./baselines
+sudo git clone https://github.com/openai/baselines.git
+sudo pip3 install -e ./baselines
 
 #Install A2C-Code
 #git clone https://github.com/njustesen/a2c_gvgai.git
 #pip3 install -e ./a2c_gvgai
 
-sh ../lib/gvgai_generator/install.sh
-npm update npm -g
+sudo sh ../lib/gvgai_generator/install.sh
+sudo npm update npm -g
