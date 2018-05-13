05525d4bdb9d706f49dcb517e4698040bb3bfffb
Niels Justesen <noju@Nielss-MacBook-Pro.local>
2018-05-13 16:21:05 -0400

    list and resume

--- a2c.py
+++ a2c.py
@@ -1,11 +1,13 @@
 import time
 import tensorflow as tf
 import argparse
+import glob
 
 from model import Model
 from runner import Runner
 from env import *
 from level_selector import *
+
 
 import uuid
 
@@ -20,13 +22,33 @@ def learn(policy, env, experiment_name, experiment_id, seed, nsteps=5, total_tim
     tf.reset_default_graph()
     set_global_seeds(seed)
 
+    # Check if experiment_id exists - then continue
+    model_path = './results/' + experiment_name + '/models/'
+    steps = 0
+    resume = False
+    for model_folder in glob.iglob(model_path + '*/'):
+
+        # Experiment id
+        id = model_folder.split('/')[-2]
+
+        if id != experiment_id:
+            continue
+
+        # Find number of steps for last model
+        for model_meta_name in glob.iglob(model_folder + '/*.meta'):
+            resume = True
+            s = int(model_meta_name.split('.meta')[0].split('/')[-1].split("-")[1])
+            if s >= steps:
+                steps = s
+
     # Log file path
     log_path = "./results/" + experiment_name + "/logs/"
     make_path(log_path)
     log_file = log_path + experiment_id + ".log"
 
     # Create log file
-    with open(log_file, "a") as myfile:
+    if not resume:
+        with open(log_file, "a") as myfile:
             line = "episodes; steps; frames; mean_score; std_score; min_score; max_score; difficulty\n"
             myfile.write(line)
 
@@ -40,7 +62,15 @@ def learn(policy, env, experiment_name, experiment_id, seed, nsteps=5, total_tim
     ac_space = env.action_space
     #num_procs = len(env.remotes) # HACK
     model = Model(policy=policy, ob_space=ob_space, ac_space=ac_space, nenvs=nenvs, nsteps=nsteps, ent_coef=ent_coef, vf_coef=vf_coef,
-        max_grad_norm=max_grad_norm, lr=lr, alpha=alpha, epsilon=epsilon, total_timesteps=total_timesteps, lrschedule=lrschedule)
+                  max_grad_norm=max_grad_norm, lr=lr, alpha=alpha, epsilon=epsilon, total_timesteps=total_timesteps, lrschedule=lrschedule)
+
+    if resume:
+        try:
+            model.load(model_path, steps)
+        except Exception as e:
+            print(e)
+            env.close()
+            return
 
     # Create parallel runner
     runner = Runner(env, model, nsteps=nsteps, gamma=gamma)
@@ -48,12 +78,22 @@ def learn(policy, env, experiment_name, experiment_id, seed, nsteps=5, total_tim
     # Training loop
     nbatch = nenvs*nsteps
     tstart = time.time()
+    next_model_save = steps + save_interval
+    start_update = steps//nbatch
     episodes = 0
-    next_model_save = save_interval
-    model.save(model_path, 0)
-    steps = 0
 
-    for update in range(1, total_timesteps//nbatch+1):
+    if not resume:
+        model.save(model_path, 0)
+    else:
+        with open(log_file, "r") as myfile:
+            for line in myfile:
+                pass
+            episodes = int(line.split(';')[0])
+            if level_selector is not None and line.split(';')[-1] != '' and hasattr(level_selector, 'difficulty'):
+                print("Restoring difficulty to " + line.split(';')[-1])
+                level_selector.difficulty = int(line.split(';')[-1])
+
+    for update in range(start_update, total_timesteps//nbatch+1):
         obs, states, rewards, masks, actions, values = runner.run()
 
         policy_loss, value_loss, policy_entropy = model.train(obs, states, rewards, masks, actions, values)
@@ -112,15 +152,16 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
     parser.add_argument('--lrschedule', help='Learning rate schedule', choices=['constant', 'linear'], default='constant')
-    parser.add_argument('--num-envs', help='Number of environments/workers to run in parallel (default=12)', type=int, default=12)
+    parser.add_argument('--num-envs', help='Number of environments/workers to run in parallel (default=12)', type=int, default=2)
     parser.add_argument('--num-timesteps', help='Number of timesteps to train the model', type=int, default=int(10e6))
     parser.add_argument('--game', help='Game name (default=zelda)', default='zelda')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--save-interval', help='Model saving interval in steps', type=int, default=int(1e6))
+    parser.add_argument('--save-interval', help='Model saving interval in steps', type=int, default=int(1e3))
     parser.add_argument('--level', help='Level (integer) to train on', type=int, default=0)
-    parser.add_argument('--repetitions', help='Number of repetions to run sequentially (default=1)', type=int, default=1)
+    parser.add_argument('--resume', help='The experiment id to resume', default='90fe9d4c-56e4-11e8-a58c-6c4008b68262')
+    parser.add_argument('--repetitions', help='Number of repetitions to run sequentially (default=1)', type=int, default=1)
     parser.add_argument('--selector', help='Level selector to use in training - will ignore the level argument if set (default: None)',
-                        choices=[None] + LevelSelector.available, default=None)
+                        choices=[None] + LevelSelector.available, default='pcg-random-3')
     args = parser.parse_args()
 
     # Gym environment name
@@ -140,7 +181,10 @@ def main():
         print("Starting experiment " + str(i+1) + " of " + str(args.repetitions+1))
 
         # Unique id for experiment
-        experiment_id = str(uuid.uuid1())
+        if args.resume is None:
+            experiment_id = str(uuid.uuid1())
+        else:
+            experiment_id = args.resume
 
         # Level selector
         level_path = './results/' + experiment_name + '/levels/' + experiment_id + '/'
@@ -177,7 +221,7 @@ def main():
 
         env.close()
 
-        print("Experiment done")
+        print("Experiment DONE")
 
 
 if __name__ == '__main__':
---
+++ list.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import argparse
+
+from level_selector import *
+from model import Model
+from runner import Runner
+from env import *
+
+from baselines.a2c.utils import make_path
+from baselines.a2c.policies import CnnPolicy
+
+from baselines.common import set_global_seeds
+from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy
+
+
+def main():
+
+    exp_path = './results/*/'
+    for exp_folder in glob.iglob(exp_path):
+
+        # Experiment name
+        exp_name = exp_folder.split('/')[-2]
+        print(exp_name)
+        steps = 0
+
+        path = os.path.join(exp_folder, 'models/*/')
+        for folder in glob.iglob(path):
+            exp_id = folder.split('/')[-2]
+
+            for model_meta_name in glob.iglob(path + '/*.meta'):
+                s = int(model_meta_name.split('.meta')[0].split('/')[-1].split("-")[1])
+                if s >= steps:
+                    steps = s
+
+            print('\t' + exp_id + '\t' + str(steps))
+
+
+if __name__ == '__main__':
+    main()
