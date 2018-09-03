67ec040de14efcc82f2685b37887fd44454c147a
Niels Justesen <njustesen@gmail.com>
2018-09-03 23:54:59 +0200

    clean up in data folder

--- auto_generate.py
+++ auto_generate.py
@@ -1,16 +1,16 @@
 from level_generator import ParamGenerator
 
 game_sizes = {
-    "aliens": [30, 11],
     "zelda": [13, 9],
     "boulderdash": [26, 13],
     "frogs": [28, 11],
     "solarfox": [10, 11]
 }
 game = "boulderdash"
-for d in [10]:
-    gen = ParamGenerator("./data/pca/{}/{}/".format(game,d), game, game_sizes[game][0], game_sizes[game][1])
-    for i in range(1000):
-        level = gen.generate([(d/10.0)], difficulty=True)
-        #print(level)
+for game in game_sizes:
+    for d in [0, 2.5, 7.5]:
+        gen = ParamGenerator("./data/test-levels/{}/{}/".format(game, d), game, game_sizes[game][0], game_sizes[game][1])
+        for i in range(1):
+            level = gen.generate([(d/10.0)], difficulty=True)
+            #print(level)
 print("Done")
